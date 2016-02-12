6fc1bd5f0409dfd5a09363a634005fcb375cad52
Nils <area51.nils@googlemail.com>
2016-02-12 13:37:04 +0100

    Add more comments.

--- hep_ipython_tools/entities.py
+++ hep_ipython_tools/entities.py
@@ -1,95 +1,72 @@
 class StoreContent:
+    """
+    Data class for an entry in a store content list.
+    """
     def __init__(self, name, number):
+        """
+        Create a new entry in a Store Content List.
+        :param name: The name of the store content.
+        :param number: The number of items in this store content.
+        """
+        #: The name of the store content.
         self.name = name
+        #: The number of items in this store content.
         self.number = number
 
 
 class StoreContentList:
+    """
+    Data class for the content of the storage for one event.
+    """
     def __init__(self, content, event_number):
+        """
+        Create a new content fr one event.
+        :param content: A list of StoreContents.
+        :param event_number: The event number this element describes.
+        """
+        #: A list of StoreContents.
         self.content = content
+        #: The event number this element describes.
         self.event_number = event_number
 
 
 class StatisticsColumn:
-    def __init__(self, name, three_column_format=False):
+    """
+    Data class for a column in the statistics view.
+    """
+    def __init__(self, name, display_name=None, three_column_format=False):
+        """
+        Create a new column for the statistics view.
+        :param name: The name of this column.
+        :param display_name: The display name of this column.
+        :param three_column_format: If the column has one or three columns.
+        """
+        #: The name of this column.
         self.name = name
+        #: If the column has one or three columns.
         self.three_column_format = three_column_format
+
+        if not display_name:
+            #: The name to show
+            self.display_name = self.name
+        else:
+            #: The name to show
+            self.display_name = display_name
 
 
 class Statistics:
+    """
+    Data class for the statistics.
+    """
     def __init__(self, columns, modules):
+        """
+        Create a new Statistics object.
+        :param columns: The list of StatisticsColumns.
+        :param modules: A list of dictionaries, each one with the same names as there are columns.
+        """
+        #: The list of StatisticsColumns.
         self.columns = columns
+        #: A list of dictionaries, each one with the same names as there are columns.
         self.modules = modules
 
 
-class ModuleStatistics:
-
-    """
-    Dictionary list holding the module statistics (like the C++ class ModuleStatistics)
-    """
-
-    def __init__(self, stats, categories):
-        #: Name property
-        self.name = stats.name
-        #: Time sum property
-        self.time_sum = self.get_dict(stats.time_sum, categories)
-        #: Time mean property
-        self.time_mean = self.get_dict(stats.time_mean, categories)
-        #: Time std property
-        self.time_stddev = self.get_dict(stats.time_stddev, categories)
-        #: Memory sum property
-        self.memory_sum = self.get_dict(stats.memory_sum, categories)
-        #: Memory mean property
-        self.memory_mean = self.get_dict(stats.memory_mean, categories)
-        #: Memory std property
-        self.memory_stddev = self.get_dict(stats.memory_stddev, categories)
-        #: Calls property
-        self.calls = self.get_dict(stats.calls, categories)
-
-    @staticmethod
-    def get_dict(function, categories):
-        """
-        Call the function on each information in the categories and return a dict
-        name -> function(value)
-        """
-        return {name: function(category) for name, category in categories}
-
-
-class Basf2CalculationQueueStatistics:
-
-    """
-    As the basf2 statistics is not pickable, we can not store it into the queue.
-    So we write a wrapper which unpacks the needed properties.
-    """
-
-    def __str__(self):
-        """ Make the str behave like before """
-        return self.str
-
-    def __repr__(self):
-        """ Make the repr behave like before """
-        return self.str
-
-    def __init__(self, statistics):
-        """ Initialize with the C++ statistics """
-        #: The module statistics list
-        self.module = []
-
-        categories = [("INIT", statistics.INIT),
-                      ("BEGIN_RUN", statistics.BEGIN_RUN),
-                      ("EVENT", statistics.EVENT),
-                      ("END_RUN", statistics.END_RUN),
-                      ("TERM", statistics.TERM),
-                      ("TOTAL", statistics.TOTAL)]
-
-        for stats in statistics.modules:
-            self.append_module_statistics(stats, categories)
-
-        self.append_module_statistics(statistics.getGlobal(), categories)
-
-        #: The str representation
-        self.str = statistics()
-
-    def append_module_statistics(self, stats, categories):
-        """ Helper function to append modulewise stats """
-        self.module.append(ModuleStatistics(stats, categories))
