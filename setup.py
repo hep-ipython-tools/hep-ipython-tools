5d232595c779b716e41b56e0d81dd5a4c1df189f
Nils <area51.nils@googlemail.com>
2016-02-17 22:49:38 +0100

    Added setup config to start the tests.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='hep_ipython_tools',
       version='1.0',
       packages=['hep_ipython_tools', "hep_ipython_tools.simple", "hep_ipython_tools.ipython_handler_basf2"],
+      tests_require=['pytest', 'pytest-cov'],
+      setup_requires=['pytest-runner'],
       )
