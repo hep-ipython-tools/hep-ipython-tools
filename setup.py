329b3a20ff7b3a5cb20b1da9738f1c4bac139c9e
Nils <area51.nils@googlemail.com>
2016-02-23 18:15:59 +0100

    Removed python 2.8

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ setup(name='hep_ipython_tools',
       version='1.0',
       packages=['hep_ipython_tools', "hep_ipython_tools.simple", "hep_ipython_tools.ipython_handler_basf2"],
       tests_require=['pytest', "pytest-cov", "mock"],
+      setup_requires=['pytest-runner']
       )
