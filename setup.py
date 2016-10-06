f8b55482b85695a18371b16acbc4ccb09c8346c3
Nils Braun <nils.braun@kit.edu>
2016-10-06 09:55:03 +0200

    Bugfix and clean up in the setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
+
 setup(name='hep_ipython_tools',
       version='1.0',
-      packages=['hep_ipython_tools', "hep_ipython_tools.simple", "hep_ipython_tools.ipython_handler_basf2"],
+      packages=['hep_ipython_tools',
+                "hep_ipython_tools.simple",
+                "hep_ipython_tools.ipython_handler_basf2",
+                "hep_ipython_tools.basic"],
       tests_require=['pytest', "pytest-cov", "mock"],
       setup_requires=['pytest-runner'],
       install_requires=['funcsigs'],
